f05542480094ff26542d45b70921750737de80a7
DESKTOP-DCNVLMS\Owner <hishida.jumpei@technopro.com>
2023-05-09 16:03:52 +0900

    HomeController,SpringUserService作成

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,63 @@
 package com.example.demo.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.SummaryRepository;
+import com.example.demo.service.RegisterService;
+import com.example.demo.service.SpringUserService;
+
+@Controller
 public class HomeController {
 
+	@Autowired
+	private SummaryRepository summaryReposiroty;
+
+	@Autowired
+	private SpringUserService springUserService;
+
+
+	// ログイン画面へ遷移する
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	// 新規登録画面へ遷移する
+	@GetMapping("/create")
+	public String create() {
+		return "create";
+
+	}
+
+	@PostMapping("/create")
+	public String create1(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "/login";
+		}
+
+		try {
+			String username = user.getName();
+			String password = user.getPassword();
+
+			springUserService.createUser(username, password);
+
+			return "redirect:/index";
+		} catch (DuplicateKeyException e) {
+			bindingResult.addError(new FieldError("user", "username", "すでに存在するユーザーです。"));
+			return "login";
+
+		}
+
+	}
 }
--- src/main/java/com/example/demo/service/SpringUserService.java
+++ src/main/java/com/example/demo/service/SpringUserService.java
@@ -1,5 +1,52 @@
 package com.example.demo.service;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
 public class SpringUserService {
+
+	@Autowired UserDetailsManager userDetailsManager;
+
+	public void createUser(String username,String password) {
+		UserBuilder builder = User.withDefaultPasswordEncoder();
+		UserDetails userDetails = 
+				builder
+				.username(username)
+				.roles("USER")
+				.password(password)
+				.disabled(false)
+				.build();
+		userDetailsManager.createUser(userDetails);
+	}
+
+		public void updateSpringUser(String username, String password) {
+			UserDetails userDetails = userDetailsManager.loadUserByUsername(username);
+
+			UserBuilder builder;
+			// 新規パスワードが渡された場合はそれを設定する。渡されていない場合は元のパスワードを設定する。
+			if (!password.equals("")) {
+				builder = User.withDefaultPasswordEncoder();
+				builder.password(password);
+			} else {
+				builder = User.builder();
+				builder.password(userDetails.getPassword());
+			}
+
+			UserDetails newUserDetails = 
+					builder
+					.username(username)
+					.authorities(userDetails.getAuthorities())
+					.disabled(false)
+					.build();
+			userDetailsManager.updateUser(newUserDetails);
+		}
 
 }
