b8a0359bf098c91c4a131bce1ce9d075fb56d72d
DESKTOP-DCNVLMS\Owner <hishida.jumpei@technopro.com>
2023-05-09 11:07:42 +0900

    WebSecurityConfig作成

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,5 +1,42 @@
 package com.example.demo;
 
-public class WebSecurityConfig {
+import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		//ログインページの制限なし
+		.antMatchers("/login").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		//ログインページ以後はログインページへ遷移するようにする
+//		.formLogin()	
+//		.loginPage("/login").permitAll()
+//		.and()
+		//ログアウトのpostが飛んで来たらログインページへ遷移する
+		.logout()
+		.logoutSuccessUrl("/login")
+		;
+	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**");
+		web.ignoring().antMatchers("/css/**", "/html/**");
+
+	}	
+	@Bean
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
 }
