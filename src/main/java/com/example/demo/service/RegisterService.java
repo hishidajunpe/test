357514ef186d9ab0534b9896f5875c02de6298ef
kojima <shoko.kojima@intra.remtiro.com>
2023-05-09 10:51:32 +0900

    RegisterServiceとapplication.propertiesの作成

--- src/main/java/com/example/demo/service/RegisterService.java
+++ src/main/java/com/example/demo/service/RegisterService.java
@@ -1,5 +1,31 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Summary;
+import com.example.demo.repository.SummaryRepository;
+
+@Service
 public class RegisterService {
 
+	@Autowired
+	private SummaryRepository summaryRepository;
+
+	//　登録
+	public Summary createSummary(Integer month,Integer day,Integer money,String genre,Boolean status){
+	Summary summary = new Summary();
+	summary.setMonth(month);
+	summary.setDay(day);
+	summary.setMoney(money);
+	summary.setGenre(genre);
+	summary.setStatus(status);
+	summaryRepository.save(summary);
+	return summary;
+	}
+
+	//　編集
+	void editSummary(Summary summary){
+	summaryRepository.save(summary);
+	}
 }
