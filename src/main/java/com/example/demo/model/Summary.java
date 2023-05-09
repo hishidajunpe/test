9322a9d9a266b2ca4dd45b7ad144a2bed30e119e
kojima <shoko.kojima@intra.remtiro.com>
2023-05-09 10:30:51 +0900

    Model(User,Summary)の作成

--- src/main/java/com/example/demo/model/Summary.java
+++ src/main/java/com/example/demo/model/Summary.java
@@ -1,5 +1,47 @@
 package com.example.demo.model;
 
-public class Summary {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
+import lombok.Data;
+
+@Entity
+@Data
+public class Summary {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@NotEmpty(message = "登録する月を入力してください")
+	@Min(1)
+	@Max(12)
+	private Integer month;
+	
+	@NotEmpty(message = "登録する日を入力してください")
+	@Min(1)
+	@Max(31)
+	private Integer day;
+	
+	@NotEmpty(message = "登録する金額を入力してください")
+	@Size(max = 9,message = "10億円以内で登録してください")
+	private Integer money;
+	
+	@NotBlank(message = "ジャンルを入力してください")
+	@Size(max = 20,message = "20文字以内で入力してください")
+	private String genre;
+	
+	//true:収入 false:支出
+	private Boolean status;
+	
+	@ManyToOne
+	private User user;
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,33 @@
 package com.example.demo.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@NotBlank(message = "名前を入力してください")
+	@Size(max = 50,message = "名前は50文字以内で入力してください")
+	private String name;
+	
+	@NotBlank(message = "パスワードを入力してください")
+	@Size(min = 8,max = 50,message = "パスワードは8文字以上50文字以内で入力してください")
+	private String password;
+	
+	@OneToMany(mappedBy="user")
+	private List<Summary> summary;
 }
