c3f4cf2238900cadf622ee8833a51e6f26e4e7c3
DESKTOP-DCNVLMS\Owner <hishida.jumpei@technopro.com>
2023-05-09 10:29:47 +0900

    SummaryRepository,UserRepository作成

--- src/main/java/com/example/demo/repository/SummaryRepository.java
+++ src/main/java/com/example/demo/repository/SummaryRepository.java
@@ -1,5 +1,28 @@
 package com.example.demo.repository;
 
-public interface SummaryRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Summary;
+
+@Repository
+public interface SummaryRepository extends JpaRepository<Summary, Integer>{
+
+	//指定されたユーザーIDからアカウント情報を取得
+	List<summary>findByUserId(Integer userId);
+	
+	//指定されたユーザーIDから〇月の収入データを取得
+	List<summary>findByUserIdAndStatusTrueAndMonthContains(Integer userId, Integer month);
+	
+	//指定されたユーザーIDから収入の全データを取得する
+	List<summary>findByUserIdAndStatusTrue(Integer userId);
+	
+	//指定されたユーザーIDから〇月の支出のデータを取得
+	List<summary> findByUserIdAndStatusFalseAndMonthContains(Integer userId, Integer month);
+	
+	//指定されたユーザーIDから支出の全データを取得する
+	List<summary> findByUserIdAndStatusFalse(Integer userId);
+	
 }
