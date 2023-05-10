f488841e05e5f159819b5076b6fb6642065e1346
DESKTOP-DCNVLMS\Owner <komata.daichi@technopro.com>
2023-05-10 13:41:33 +0900

    Account,accountFormクラスの作成

---
+++ src/main/java/com/example/demo/model/Account.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Account {
+    @Id
+    @Column(length = 80)
+    private String username;
+    
+    private Boolean active;
+    
+    @OneToOne
+    private User user;
+    
+}
