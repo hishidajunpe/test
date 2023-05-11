79094f869325492580c30c05a94de451d736bda3
DESKTOP-DCNVLMS\Owner <komata.daichi@technopro.com>
2023-05-11 10:58:01 +0900

    #UserController登録ページに遷移可能、支出と収入の合計をフォームからの入力で表示可能

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -23,80 +23,97 @@ import com.example.demo.service.RegisterService;
 @Controller
 public class UserController {
 	@ModelAttribute("user")
-    public User currentUser(Account account) {
-        return account.getUser();
-    }
-//	@ModelAttribute("summary")
-	
-	
-	
+	public User currentUser(Account account) {
+		return account.getUser();
+	}
+	//	@ModelAttribute("summary")
+
+
+
 	@Autowired RegisterService registerService;
-		@GetMapping("/")
+	@GetMapping("/")
 	//	トップページに行き、収入の合計とそのテーブル、支出の合計とそのテーブルを表示したい。
-		String index(Model model){
-////	//	収入の合計を表示
-//			int sumIncome = 0;
-//			
-//			sumIncome=registerService.getSumIncome(year, month);
-//			model.addAttribute("sumIncome", sumIncome);
-////	//	支出の合計を表示
-//			int sumOutcome = 0;
-//			sumOutcome=registerService.getSumOutcome(year, month);
-//			model.addAttribute("sumOutcome", sumOutcome);
-			
-//	//	収入のテーブル情報をモデルに登録
-//		model.addAttribute("income",income);
-//
-//		Summary outcome = registerService.getOutcomeMonth
-//	//	支出のテーブル情報をモデルに登録
-//		model.addAttribute("outcome",outcome);
+	String index(Model model){
+		
 		return "index";
-		}
+	}
+//////	収入の合計を表示
+	//			int sumIncome = 0;
+	//			sumIncome=registerService.getSumIncome(year, month);
+	//			model.addAttribute("sumIncome", sumIncome);
+
+	////	//	支出の合計を表示
+	//			int sumOutcome = 0;
+	//			sumOutcome=registerService.getSumOutcome(year, month);
+	//			model.addAttribute("sumOutcome", sumOutcome);
+
+	//	//	収入のテーブル情報をモデルに登録
+	//		model.addAttribute("income",income);
+	//
+	//		Summary outcome = registerService.getOutcomeMonth
+	//	//	支出のテーブル情報をモデルに登録
+	//		model.addAttribute("outcome",outcome);
+	
+//	収入の合計を表示
+	@PostMapping("/")
+	public String sumIncome(@RequestParam Integer year,Integer month,Model model) {
+		int sumIncome = 0;
+		sumIncome=registerService.getSumIncome(year, month);
+		model.addAttribute("sumIncome", sumIncome);
+		return "index";
+	}
+//	支出の合計を表示
+	@PostMapping("/")
+	public String sumOut(@RequestParam Integer year,Integer month,Model model) {
+		int sumOutcome = 0;
+					sumOutcome=registerService.getSumOutcome(year, month);
+					model.addAttribute("sumOutcome", sumOutcome);
+		return "index";
+	}
+	
+	@PostMapping("/")
+	public String tableIncome(@RequestParam Integer year,Integer month,Model model) {
+			List<Summary> summary = registerService.getIncomeMonth(year, month);
+			model.addAttribute("tableIncome", summary);
 		
-//		@PostMapping("/")
-//		public String index() {
-//			return "index";
-//		}@PostMapping("/")
-//		public String index() {
-//			return "index";
-//		}
-//		@PostMapping("/")
-//		public String index() {
-//			return "index";
-//		}
-//		@PostMapping("/")
-//		public String index() {
-//			return "index";
-//		}
-//
-//	//	登録ページに移動する。
-//		@GetMapping("/register")
-//		String create(){
-//		return "register";
-//		}
-//
-//		//	登録内容をDBへ反映する。失敗したら登録ページに戻る。成功したら再度登録ページに
-//		@PostMapping("/register")
-//		String createSummary(@ModelAttribute Summary summary,BindingResult  bindingResult){
-//		 if (bindingResult.hasErrors()) {
-//		            return "/register";
-//		        }
-//		       registerService.createSummary(summary);
-//		return "redirect:/register";
-//		}
-		//	編集ページに移動する。
-		@GetMapping("/edit")
-		String edit(){
+		return "index";
+	}
+	@PostMapping("/")
+	public String tableOutcome(@RequestParam Integer year,Integer month,Model model) {
+		List<Summary> summary = registerService.getOutcomeMonth(year, month);
+		model.addAttribute("tableOutcome", summary);
+	
+		return "index";
+	}
+
+	//	登録ページに移動する。
+	@GetMapping("/register")
+	String create(){
+		return "register";
+	}
+
+	//	登録内容をDBへ反映する。失敗したら登録ページに戻る。成功したら再度登録ページに
+	@PostMapping("/register")
+	String createSummary(@ModelAttribute Summary summary,BindingResult  bindingResult){
+		if (bindingResult.hasErrors()) {
+			return "/register";
+		}
+		registerService.createSummary(summary);
+		return "redirect:/register";
+	}
+	//	編集ページに移動する。
+	@GetMapping("/edit")
+	String edit(){
 		return "edit";
+	}
+
+	//	編集内容をDBへ反映させる。失敗したら編集画面に戻る。成功したらトップ（index）にリダイレクトする。
+	@PostMapping("/edit")
+	String editSummary(@ModelAttribute Summary summary,BindingResult  bindingResult,Integer month,Integer day,Integer money,String genre,Boolean status ){
+		if (bindingResult.hasErrors()) {
+			return "/edit";
 		}
-		
-		//	編集内容をDBへ反映させる。失敗したら編集画面に戻る。成功したらトップ（index）にリダイレクトする。
-		@PostMapping("/edit")
-		String editSummary(@ModelAttribute Summary summary,BindingResult  bindingResult,Integer month,Integer day,Integer money,String genre,Boolean status ){
-		 if (bindingResult.hasErrors()) {
-		            return "/edit";
-		        }
-		       registerService.editSummary(summary);
+		registerService.editSummary(summary);
 		return "redirect:/";
-		}
+	}
 }
