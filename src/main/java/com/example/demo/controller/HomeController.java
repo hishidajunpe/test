eaab11a77bbc1130b10385a9f54c73c6a99d7f3a
kojima <shoko.kojima@intra.remtiro.com>
2023-05-12 17:14:07 +0900

    ログイン画面と新規登録のバリデーション追加しました

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -5,38 +5,31 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.model.AccountForm;
-//import com.example.demo.model.User;
-//import com.example.demo.repository.SummaryRepository;
-//import com.example.demo.service.RegisterService;
-//import com.example.demo.service.SpringUserService;
 import com.example.demo.service.UserService;
 
 @Controller
 public class HomeController {
 
-//	@Autowired
-//	private SummaryRepository summaryReposiroty;
-//
-//	@Autowired
-//	private SpringUserService springUserService;
-//	
 	@Autowired
 	private UserService userService;
 
 
 	// ログイン画面へ遷移する
 	@GetMapping("/login")
-	public String login() {
-		return "login";
+	public String login(@Valid AccountForm accountForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			System.out.println(1);
+			return "/login";
+		}
+		return "redirect:/";
 	}
+
 	@PostMapping("/login")
 	public String index() {
 		return "/";
@@ -54,12 +47,17 @@ public class HomeController {
 	public String create(@Valid AccountForm accountForm, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			System.out.println(1);
-			return "/login";
+			return "/create";
 		}
 
 		try {
 			String username = accountForm.getName();
+			System.out.println(2);
 			String password = accountForm.getPassword();
+			System.out.println(3);
+
+//			springUserService.createUser(username, password);
+			System.out.println(4);
 			userService.saveUser(username, password);
 			return "redirect:/";
 		} catch (DuplicateKeyException e) {
